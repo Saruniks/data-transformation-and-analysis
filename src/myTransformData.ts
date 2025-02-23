a793ae3283d812f37a2e6b5cdc3ba7e536a4b81a
sarunas.gincas <sarunikss@gmail.com>
2025-02-23 17:37:23 +0200

    Filter artists who have songs

--- src/myTransformData.ts
+++ src/myTransformData.ts
@@ -11,12 +11,11 @@ log4js.configure({
     appenders: { console: { type: 'console' } },
     categories: { default: { appenders: ['console'], level: 'debug' } },
 });
-
 const logger = log4js.getLogger();
 
 function parseCSVFile(filePath: string) {
     const file = fs.readFileSync(filePath, 'utf8');
-    logger.debug('Path: ', filePath, '.Length: ', file.length);
+    logger.debug('Path:', filePath, 'Length:', file.length);
 
     return parse(file, {
         trim: false,
@@ -25,21 +24,26 @@ function parseCSVFile(filePath: string) {
     });
 }
 
+interface Artist {
+    id: string;
+    // [key: string]: unknown;
+}
+
 interface Track {
     name: string;
     duration_ms: number;
+    id_artists: string;
     // [key: string]: unknown;
 }
 
 
 function main() {
     try {
-        const artists = parseCSVFile(artistsFilePath);
+        const artists: Artist[] = parseCSVFile(artistsFilePath);
         logger.debug('Example artists row:', artists[0]);
 
         const tracks: Track[] = parseCSVFile(tracksFilePath);
         logger.debug('Example tracks row:', tracks[0]);
-
 
         // Simplicity over memory usage and performance
         const filteredTracks = tracks.filter((t) => {
@@ -48,10 +52,23 @@ function main() {
             return true;
         });
 
+        const artistsSet = new Set();
+        filteredTracks.forEach((t) => {
+            logger.trace('id_artists:', t.id_artists);
+            const artistsIds = JSON.parse(t.id_artists.replace(/'/g, '"'));
+            logger.trace('parsed id_artists:', artistsIds);
+
+            artistsIds.forEach((id: string) => {
+                logger.trace('id: ', id);
+                artistsSet.add(id);
+            });
+        });
+        const filteredArtists = artists.filter((a) => artistsSet.has(a.id));
+
         const tracksCopy = path.resolve(__dirname, '../data/tracksCopy.csv');
         const artistsCopy = path.resolve(__dirname, '../data/artistsCopy.csv');
         fs.writeFileSync(tracksCopy, stringify(filteredTracks, { header: true }));
-        fs.writeFileSync(artistsCopy, stringify(artists, { header: true }));
+        fs.writeFileSync(artistsCopy, stringify(filteredArtists, { header: true }));
     } catch (err) {
         logger.error(err);
     }
@@ -61,3 +78,5 @@ main();
 
 // test nr 1: test if has no name for example
 // test nr 2: proper duration filtering
+// test nr 3: are array of artists_id parsed correctly
+// test nr 4: proper artist id filtering? So many filtered out...
