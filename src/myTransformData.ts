9ba375abc1fff62380ce81d90d680ff256cb4d1c
sarunas.gincas <sarunikss@gmail.com>
2025-02-23 16:25:38 +0200

    Parse and write back files. Note: Why artistsCopy.csv is smaller than artists.csv?

---
+++ src/myTransformData.ts
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import path from 'path';
+import log4js from 'log4js';
+import { parse } from 'csv-parse/sync';
+import { stringify } from 'csv-stringify/sync';
+
+const tracksFilePath = path.resolve(__dirname, '../data/tracks.csv');
+const artistsFilePath = path.resolve(__dirname, '../data/artists.csv');
+
+log4js.configure({
+    appenders: { console: { type: 'console' } },
+    categories: { default: { appenders: ['console'], level: 'debug' } },
+});
+
+const logger = log4js.getLogger();
+
+function parseCSVFile(filePath: string) {
+    const file = fs.readFileSync(filePath, 'utf8');
+    logger.debug('Path: ', filePath, '.Length: ', file.length);
+
+    return parse(file, {
+        trim: false,
+        skip_empty_lines: false,
+        columns: true
+    });
+}
+
+function main() {
+    try {
+        const artists = parseCSVFile(artistsFilePath);
+        logger.debug('Example artists row:', artists[0]);
+
+        const tracks = parseCSVFile(tracksFilePath);
+        logger.debug('Example tracks row:', tracks[0]);
+
+        tracks.filter((t) => {
+            if (!t.name) return false;
+            return true;
+        });
+
+        const tracksCopy = path.resolve(__dirname, '../data/tracksCopy.csv');
+        const artistsCopy = path.resolve(__dirname, '../data/artistsCopy.csv');
+        fs.writeFileSync(tracksCopy, stringify(tracks, { header: true }));
+        fs.writeFileSync(artistsCopy, stringify(artists, { header: true }));
+    } catch (err) {
+        logger.error(err);
+    }
+}
+
+main();
+
+// test nr 1: test if has no name for example
