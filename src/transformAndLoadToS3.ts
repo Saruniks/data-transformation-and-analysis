9246c19a526af358ce8ef6f2c4f45971fefb58cc
sarunas.gincas <sarunikss@gmail.com>
2025-02-24 22:43:40 +0200

    Refactor

--- src/transformAndLoadToS3.ts
+++ src/transformAndLoadToS3.ts
@@ -5,30 +5,35 @@ import { downloadSpotifyDataset } from './downloadFromKaggle';
 import { filterArtists, filterTracks, transformTracks } from './transform';
 
 async function main() {
+    let tmpFiles;
     try {
-        const tmpFiles = await downloadSpotifyDataset();
+        tmpFiles = await downloadSpotifyDataset();
+        
+        const [artists, tracks] = await Promise.all([
+            parseCSVFile(`${tmpFiles.name}/artists.csv`),
+            parseCSVFile(`${tmpFiles.name}/tracks.csv`)
+        ]);
 
-        const artists = await parseCSVFile(`${tmpFiles.name}/artists.csv`);
         logger.trace('Example artists row:', artists[0]);
-
-        const tracks = await parseCSVFile(`${tmpFiles.name}/tracks.csv`);
         logger.trace('Example tracks row:', tracks[0]);
 
         logger.info('Transforming data...');
         const filteredTracks = filterTracks(tracks);
-
         const filteredArtists = filterArtists(artists, filteredTracks);
-
         const transformedFilteredTracks = transformTracks(filteredTracks);
 
         logger.info('Uploading data to S3...');
-
-        uploadAsCSVFileToS3(transformedFilteredTracks, 'transformedTracks.csv');
-        uploadAsCSVFileToS3(filteredArtists, 'transformedArtists.csv');
+        
+        await Promise.all([
+            uploadAsCSVFileToS3(transformedFilteredTracks, 'transformedTracks.csv'),
+            uploadAsCSVFileToS3(filteredArtists, 'transformedArtists.csv')
+        ]);
 
         logger.info('Data uploaded to S3 successfully');
     } catch (err) {
         logger.error(err);
+    } finally {
+        tmpFiles?.removeCallback();
     }
 }
 
